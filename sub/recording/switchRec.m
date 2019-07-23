5fa4aa8bf9bd793d55c74299439f625b960e8203
Speck <ks56cyvu@dom.uni-leipzig.de>
2019-07-23 14:58:32 +0200

    global removes

--- sub/recording/switchRec.m
+++ sub/recording/switchRec.m
@@ -1,23 +1,21 @@
-% switchRec - changes recording state according to current state.
+% switchRec - changes recording state depending on current state.
 
 function switchRec(object_handle, event)   
     global flag; 
     global timesofrec;
     global button;
     global vidopen;
-    % global tmr;
     
-    if flag.Record == 0 % in case of no recording in progress
+    % in case of no recording in progress
+    if flag.Record == 0 
         flag.Record = 1; % starts recording
         flag.Preview = 0; % Preview already initialized 
-        %start(tmr)
-        %vidopen = 1;
         set(button,'string','Stop'); % change string on Recording-Button
-        
-    elseif flag.Record == 1 % in case of recording in progress
+    
+    % in case of recording in progress
+    elseif flag.Record == 1 
         flag.Record = 0; % stops recording
         flag.Preview = 1; % preview preparation for next recording
-        %stop(tmr)
         set(button,'string','Record'); % change string on Recording-Button
         timesofrec = timesofrec + 1;
         vidopen = 0;
